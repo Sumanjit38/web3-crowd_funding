358f7aea24c96b6e42d3500224af32eb492d3343
Sumanjit38 <sumanjit38@gmail.com>
2023-08-11 18:37:17 +0530

    Create Campaign

---
+++ client/src/context/index.jsx
@@ -0,0 +1,53 @@
+import React, { useContext, createContext } from 'react';
+
+import { useAddress, useContract, useMetamask, useContractWrite } from '@thirdweb-dev/react';
+import { ethers } from 'ethers';
+//import { EditionMetadataWithOwnerOutputSchema } from '@thirdweb-dev/sdk';
+
+const StateContext = createContext();
+
+export const StateContextProvider = ({ children }) => {
+    const { contract } = useContract('0x5FbDB2315678afecb367f032d93F642f64180aa3');
+    const { mutateAsync: createCampaign } = useContractWrite(contract, 'createCampaign');
+
+    const address = useAddress();
+    const connect = useMetamask();
+
+    const publishCampaign = async (form) => {
+        try {
+          const data = await createCampaign({
+                    args: [
+                        address, // owner
+                        form.title, // title
+                        form.description, // description
+                        form.target,
+                        new Date(form.deadline).getTime(), // deadline,
+                        form.image,
+                    ],
+                });
+    
+          console.log("contract call success", data)
+        } catch (error) {
+          console.log("contract call failure", error)
+        }
+      }
+
+      return (
+        <StateContext.Provider
+          value={{ 
+            address,
+            contract,
+            connect,
+            createCampaign: publishCampaign,
+            //getCampaigns,
+            //getUserCampaigns,
+            //donate,
+            //getDonations
+          }}
+        >
+          {children}
+        </StateContext.Provider>
+      )
+}
+
+export const useStateContext = () => useContext(StateContext);
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDom from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
 
+import { StateContextProvider } from './context';
 import App from './App';
 import './index.css';
 
@@ -11,7 +12,10 @@ const root = ReactDom.createRoot(document.getElementById('root'));
 root.render(
     <ThirdwebProvider desiredChainId={ChainId.Hardhat}>
         <Router>
-            <App />
+            <StateContextProvider>
+                <App />
+            </StateContextProvider>
+            
         </Router>
     </ThirdwebProvider>
 )
