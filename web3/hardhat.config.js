d8c01dfaddc92d2081c930bb61de55e255d75266
Sumanjit38 <sumanjit38@gmail.com>
2023-08-11 13:56:48 +0530

    Create Smart Contract

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -5,6 +5,14 @@ module.exports = {
   
   solidity: {
     version: "0.8.17",
+    defaultNetwork: 'hardhat',
+    chainId: 31337,
+    networks: {
+      hardhat: {
+        url: 'http://127.0.0.1:8545/',
+        accounts: [`0x${process.env.PRIVATE_KEY}`]
+      }
+    },
     settings: {
       optimizer: {
         enabled: true,
